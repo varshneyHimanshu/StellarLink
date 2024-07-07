12221fa14d44170659abe1fb8667be3ab8a502f5
Jahanvi-Varshney <jahanvi.varshney_mca22@gla.ac.in>
2024-07-07 16:28:45 +0530

    ftr

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -34,9 +34,7 @@ const Chat = () => {
 
   // Connect to Socket.io
   useEffect(() => {
-    socket.current = io("//stellarlinksocket.onrender.com",{
-      transport:["polling"]
-    });
+    socket.current = io("ws://stellarlinksocket.onrender.com");
     socket.current.emit("new-user-add", user._id);
     socket.current.on("get-users", (users) => {
       setOnlineUsers(users);
